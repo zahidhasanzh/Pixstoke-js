c24a24e78a2c852514faaf780856c3b3b9cf887b
zahidhasanzh <zahidhasanzh000@gmail.com>
2024-08-27 12:50:55 +0600

    light and dark mode js done

--- js/theme.js
+++ js/theme.js
@@ -1,6 +1,28 @@
 /**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
+ * @copyright Zahid Hassan 2024
  */
 
 "use strict";
+
+const $HTMl = document.documentElement;
+
+let isDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+
+if(sessionStorage.getItem("theme")){
+    $HTMl.dataset.theme = sessionStorage.getItem("theme")
+}else{
+    $HTMl.dataset.theme = isDark ? "dark" : "light"
+}
+
+const changeTheme = function(){
+    isDark = sessionStorage.getItem("theme")
+    console.log(isDark);
+    sessionStorage.setItem("theme", isDark === "light" ? "dark" : "light");
+    $HTMl.dataset.theme = sessionStorage.getItem("theme")
+}
+
+window.addEventListener("load", () => {
+    const $themeBtn = document.querySelector("[data-theme-toggler]");
+    $themeBtn.addEventListener("click", changeTheme)
+})
