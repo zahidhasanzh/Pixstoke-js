38e27e713e3423ceece9625065f907c96cc14841
zahidhasanzh <zahidhasanzh000@gmail.com>
2024-08-27 15:41:42 +0600

    ripple effect btn doen

--- js/global.js
+++ js/global.js
@@ -4,6 +4,9 @@
 
 "use strict";
 
+// import
+import { ripple } from "./utils/ripple.js";
+
 // Header on-scroll state
 
 const $header = document.querySelector("[data-header]");
@@ -11,3 +14,10 @@ const $header = document.querySelector("[data-header]");
 window.addEventListener("scroll", () => {
     $header.classList[window.scrollY > 50 ? "add" : "remove"]("active")
 })
+
+
+// add ripple effect
+
+const $rippleElems = document.querySelectorAll("[data-ripple]");
+
+$rippleElems.forEach($rippleElem => ripple($rippleElem))
--- js/utils/ripple.js
+++ js/utils/ripple.js
@@ -1,6 +1,41 @@
 /**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
+ * @copyright Zahid Hassan 2024
  */
 
 "use strict";
+
+// add ripple effect on an element
+export const ripple = function($rippleElem){
+    $rippleElem.addEventListener("pointerdown", function(e){
+        e.stopImmediatePropagation()
+        console.log(e);
+
+        const $ripple = document.createElement("div");
+        $ripple.classList.add("ripple");
+
+        this.appendChild($ripple);
+
+        const removeRipple = () => {
+            $ripple.animate({
+                opacity: 0
+            }, {fill: "forwards", duration: 200})
+
+            setTimeout(() => {
+                $ripple.remove()
+            }, 1000);
+        }
+        this.addEventListener("pointerup", removeRipple);
+        this.addEventListener("pointerleave", removeRipple);
+
+
+        const isNotIconButton = !this.classList.contains("icon-btn");
+
+        if(isNotIconButton){
+            const rippleSize = Math.max(this.clientWidth, this.clientHeight);
+            $ripple.style.top = `${e.layerY}px`;
+            $ripple.style.left = `${e.layerX}px`;
+            $ripple.style.width = `${rippleSize}px`
+            $ripple.style.height = `${rippleSize}px`
+        }
+    })
+}
