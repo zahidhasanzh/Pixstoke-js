79a3f883824eb9451ece294a107e943e36988247
zahidhasanzh <zahidhasanzh000@gmail.com>
2024-08-27 14:28:42 +0600

    Header scroll state done

--- js/global.js
+++ js/global.js
@@ -1,6 +1,13 @@
 /**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
+ * @copyright Zahid Hassan 2024
  */
 
 "use strict";
+
+// Header on-scroll state
+
+const $header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", () => {
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active")
+})
