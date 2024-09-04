252a00b6da3d1d41c84abf79269004b310713a8e
zahidhasanzh <zahidhasanzh000@gmail.com>
2024-09-04 15:26:50 +0600

    - Redner collections in collection page

--- pages/collections/collection.js
+++ pages/collections/collection.js
@@ -1,6 +1,47 @@
 /**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
+ * @copyright Zahid Hassan 2024
  */
 
 "use strict";
+//import
+
+import { client } from "../../js/api_configure.js";
+import { collectionCard } from "../../js/collection_card.js";
+
+// render featured collections
+
+const $collectionGrid = document.querySelector("[data-collection-grid]");
+const perPage = 36;
+let currentPage = 1;
+let totalPage = 0;
+
+const loadCollections = function (page){
+    client.collections.featured({per_page: perPage, page: page}, data => {
+       
+        totalPage = Math.ceil(data.total_results / perPage);
+       
+        data.collections.forEach(collecton => {
+            const $collectionCard = collectionCard(collecton);
+            $collectionGrid.appendChild($collectionCard)
+        })
+
+        isloaded = true;
+      (currentPage >= totalPage) && ($loader.style.display = 'none')
+      
+    })
+}
+loadCollections(currentPage)
+
+//load more collection
+
+const $loader = document.querySelector("[data-loader]");
+let isloaded = false;
+const loadMore = function() {
+    if($loader.getBoundingClientRect().top < (window.innerHeight * 2) && currentPage <= totalPage && isloaded){
+        currentPage++;
+        loadCollections(currentPage);
+        isloaded = false;
+    }
+}
+
+window.addEventListener("scroll", loadMore)
--- pages/videos/video_detail.js
+++ pages/videos/video_detail.js
@@ -1,6 +1,103 @@
 /**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
+ * @copyright Zahid Hassan 2024
  */
-
 "use strict";
+
+// import
+
+import { client } from "../../js/api_configure.js";
+import { ripple } from "../../js/utils/ripple.js";
+import { menu } from "../../js/menu.js";
+import { favorite } from "../../js/favorite.js";
+
+// add ripple effect
+
+const $rippleElems = document.querySelectorAll("[data-ripple]");
+
+$rippleElems.forEach($rippleElem => ripple($rippleElem));
+
+
+//page transition
+
+window.addEventListener("loadstart", function(){
+    document.body.style.opacity = "0"
+ })
+ window.addEventListener("DOMContentLoaded", function(){
+    document.body.style.opacity = "1"
+ })
+
+//  menu toggle
+
+const $menuWrappers = document.querySelectorAll("[data-menu-wrapper]");
+
+$menuWrappers.forEach($menuWrapper => {
+    menu($menuWrapper)
+})
+
+// add to favorite
+const favoriteVideos = JSON.parse(window.localStorage.getItem("favorite")).videos
+const $favoriteBtn = document.querySelector("[data-add-favorite]");
+const videoId = window.location.search.split("=")[1];
+
+$favoriteBtn.classList[favoriteVideos[videoId] ? "add" : "remove"]("active");
+
+favorite($favoriteBtn, "videos", videoId);
+
+
+
+const $detailWrapper = document.querySelector("[data-detail-wrapper]");
+const $downloadLink = document.querySelector("[data-download-link]");
+const $downloadMenu = document.querySelector("[data-download-menu]");
+
+client.videos.detail(videoId, data => {
+   
+    console.log(data);
+   const {
+    width,
+    height,
+    image,
+    user: {name: author},
+    video_files
+
+   } = data;
+
+   const hdVideo = video_files.find(item => item.quality === "hd");
+   const {file_type, link} = hdVideo;
+
+   $downloadLink.href = link;
+
+
+   video_files.forEach(item => {
+        const {
+            width,
+            height,
+            quality,
+            link
+        } = item
+
+      $downloadMenu.innerHTML += `
+
+         <a href="${link}" download class="menu-item">
+            <span class="label-large text">${quality.toUpperCase()}</span>
+
+            <span class="label-large trailing-text">${width}x${height}</span>
+
+            <div class="state-layer"></div>
+        </a>
+      
+      `
+   })
+
+   $detailWrapper.innerHTML = `
+   
+    <div class="detail-banner" style="aspect-ratio: ${width} / ${height};">
+        <video poster="${image}" controls class="img-cover" data-video>
+            <source src="${link}" type="${file_type}">
+        </video>
+    </div>
+    <p class="title-small">Video by <span class="primary">${author}</span></p>
+   
+   `;
+
+});
+
